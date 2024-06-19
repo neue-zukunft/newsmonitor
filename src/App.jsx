e9aa9148cf1ae8fd7a529a5decea11e99cd97f5e
Norman Schumann <norman.schumann@key-work.de>
2024-06-19 18:21:30 +0200

    Update App.css with new styling for headline blocks and media blocks

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,11 @@ import { Tooltip } from 'react-tooltip'
 
 function App() {
   const containers = Array.from({ length: 50 }, (_, index) => index + 1);
-  const mediaBlocks = ["Tagesschau", "heute", "Spiegel", "Zeit", "SZ", "taz", "FAZ", "rnd", "Welt"]
+  const mediaBlocks = ["Tagesschau", "heute", "Spiegel", "Zeit", "SZ", "taz", "FAZ", "rnd", "Welt"].sort(() => Math.random()-0.5)
   
   const handleClick = (myLink) => () => {
     window.location.href=myLink;
   }
-  
-console.log(data);
 
 const latestTimestamp = data.reduce((latest, item) => {
   const timestamp = new Date(item.date).getTime();
@@ -32,65 +30,28 @@ const formattedDataLength = (data.length / 450 * 100).toFixed(1) + '%';
 
   return (
     <>
-    <div style={{
-          display: 'inline',
-          fontFamily: 'ProtestGroteskBold',
-          fontStyle: 'bold',
-          textAlign: 'left',
-          float: 'left'
-        }}>
-      <h1
-        style={{
-          display: 'inline',
-          fontFamily: 'ProtestGroteskBold',
-          fontStyle: 'bold',
-          textAlign: 'left'
-        }}
-      >Newsmonitor</h1>
+    <div className='headlineblockLeft'>
+      <h1 className='h1Left'>
+        Newsmonitor
+      </h1>
       
-      <h2 style={{
-        display: 'inline',
-        textAlign: 'left'
-      }}>alpha</h2>
-      </div>
+      <h2 className='h2Left'>
+        alpha
+      </h2>
+    </div>
 
-<div style={{
-  display: 'inline',
-  textAlign: 'right',
-  float: 'right',
-  clear:'right'
-}}>
-      <div style={{
-        display: 'inline',
-        textAlign: 'right',
-        fontFamily: 'ProtestGrotesk',
-        fontStyle: 'bold'
-      }}>
+    <div className='headlineblockRight'>
+      <div className='angebotRight'>
         Ein&nbsp;Angebot&nbsp;von&nbsp;&nbsp;</div>
       <img 
-      src={logoUrl}
-      alt='Logo'
-      style={{
-        display: 'inline',
-        textAlign: 'right',
-        width: '100px',
-        fontFamily: 'ProtestGrotesk'
-      }}
+        src={logoUrl}
+        alt='Logo'
+        className='logo'
       ></img>
-      </div>
+    </div>
 
-      <div
-      style={{
-        display:'inline-block'
-      }}>
-      <p
-      style={{
-        width: '90vw',
-        //border: '1px solid black',
-        //backgroundColor: 'lightgrey',
-        fontFamily: 'ProtestGrotesk',
-        textAlign: 'block'
-      }}>
+      <div style={{display:'inline-block'}}>
+      <p className='fristBlock'>
         Wieviel News zur Klimakrise finden sich jetzt gerade auf den ersten 50 Positionen der Startseiten von Nachrichtenwebsites? Mehr zum Hintergrund und Methodik unterhalb. Letzte Aktualisierung: {formattedTimestamp}.</p>
       {/* // Hier werden auf den angegeben Medienprotalen in absteigender Reihung die ersten fünfzig Artikelpositionen angezeigt. 
       // Die grünen Positionen markieren die Artikel, bei denen die Buchstabenfolge "Klima" im Titel oder im Text vorkommt. Das können 
@@ -101,30 +62,13 @@ const formattedDataLength = (data.length / 450 * 100).toFixed(1) + '%';
       // Die letzte Aktualisierung war .</p> */}
       </div>
 
-      <div
-      style={{
-        display:'inline'
-      }}>
-      <p
-      style={{
-        width: '90vw',
-        //border: '1px solid black',
-        //backgroundColor: 'lightgrey',
-        fontFamily: 'ProtestGroteskBold',
-        backgroundColor: 'rgb(255,100,0)',
-        color: 'white'
-      }}>
-        Zur Zeit handeln {formattedDataLength} der Beiträge auf {mediaBlocks.length} News-Starseiten von der Klimakrise.
+      <div style={{display:'inline'}}>
+        <p className='secondBlock'>
+          Zur Zeit handeln {formattedDataLength} der Beiträge auf {mediaBlocks.length} News-Starseiten von der Klimakrise.
         </p>
       </div>
 
-      <div style={{
-        display: 'flex',
-        //width: '100%',
-        width: '90vw',
-        border: '1px solid black',
-        overflow: 'auto'
-            }}>
+      <div className='tableBlock'>
 
             {mediaBlocks.map(media => {
         const filteredData = data.filter(d => d.name === media);
@@ -138,22 +82,11 @@ const formattedDataLength = (data.length / 450 * 100).toFixed(1) + '%';
 
         return (
 
-        <div key={media} className='mediaBlock'
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100px',
-          padding: '10px',
-          margin: '10px',
-          border: '1px solid black'
-        }}
-        >
-          <h2>{media}</h2>
+        <div key={media} className='mediaBlock'>
+          <p className='mediaTitle'>{media}</p>
           {containers.map(container => {
             const isRed = filteredData.map(data => data.position).includes(container);
-            const links = filteredData.map(data => data.url);
+            //const links = filteredData.map(data => data.url);
 
             //const initialLinks = filteredData.map(data => data.url);
             //const [links, setLinks] = useState(filteredData.map((data) => data.url));
@@ -164,18 +97,16 @@ const formattedDataLength = (data.length / 450 * 100).toFixed(1) + '%';
               <div 
                 key={container}
                 data-tooltip-id={isRed ? "my-tooltip" : null}
-                data-tooltip-content={isRed ? filledContainers[
-                container-1].title : null}
-                onMouseEnter={isRed ? () => console.log('Test')
-                 : null}
+                data-tooltip-content={isRed ? filledContainers[container-1].title : null}
+                //onMouseEnter={isRed ? () => console.log('Test') : null}
                 //onClick={isRed ? handleClners[container-1].url) : null}
                 style={{
                   backgroundColor: isRed ? 'rgb(255,100,0)' : 'rgb(234,232,228)',
                   padding: '10px',
                   margin: '0.5px',
-                  width: '70px'
-                }}
-              >
+                  width: '70px',
+                  height: '0.5px',
+                }}>
                 <Tooltip id="my-tooltip" />
               </div>
             );
